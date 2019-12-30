376b783ee5dedcd57b3e7cc0ccc4bf5431c722c0
Harrygiel <harrygiel@gmail.com>
2019-12-30 12:40:56 +0100

    Furniture reworked Added a homemade wikilib

--- lib/furniture.py
+++ lib/furniture.py
@@ -2,6 +2,7 @@
 
 import os, json, sys
 import lib.extractlib as lib
+import lib.wikilib as wiki
 
 def furniture_info(furniture_json):
 #Get every information of a furniture:
@@ -61,40 +62,63 @@ def get_full_furniture_list():
 
 
 def furniture_wiki():
+	table_keys = ['Name', 'Description', 'Tags', 'Base maximum', 'Cost', 'Bonus', 'Requirement'] 
+	table_lines = []
 	result_list = lib.get_json("data/", "furniture")
+	for json_value in result_list:
+		furniture_json = furniture_info(json_value)
+		table_line = []
+		# NAME part
+		if furniture_json.get('sym') != None:
+			table_line.append('| <span id="' + str(furniture_json['id']) + '">' + furniture_json['sym'] + '[[' +  str(furniture_json['name']) + ']]</span>')
+		else:
+			table_line.append('| <span id="' + str(furniture_json['id']) + '">[[' +  str(furniture_json['name']) + ']]</span>')
+
+		# Description part
+		table_line.append(str(furniture_json['desc']))
+
+		# Tags part
+		tmp_cell = ""
+		for tag in furniture_json['tags']:
+			tmp_cell += str(tag) + "<br/>"
+		table_line.append(str(tmp_cell))
+
+		# Base maximum part
+		table_line.append(str(furniture_json['base_max'] ))
+
+		# Cost part
+		tmp_cell = ""
+		for mod_key in furniture_json['cost']:
+			tmp_cell += (str(mod_key) + ": " + str(furniture_json['cost'][mod_key]) + '<br/>')
+		table_line.append(str(tmp_cell))
+		
+		# Bonus part
+		tmp_cell = ""
+		for mod_key in furniture_json['mod']:
+			tmp_cell += (str(mod_key) + ": " + str(furniture_json['mod'][mod_key]) + '<br/>')
+		table_line.append(str(tmp_cell))
+
+		# Requirement part
+		table_line.append(str(furniture_json['require'].replace("&&", "<br/>").replace("||", "<br/>OR<br/>")))
+
+		table_lines.append(table_line)
+
 	with open("furnitures.txt", "w", encoding="UTF-8") as wiki_dump:
-		wiki_dump.write('{| class="wikitable sortable"\n')
-		wiki_dump.write('|-\n')
-		wiki_dump.write('! Name !! Description !! Tags !! Base maximum !! Cost !! Bonus !! Requirement \n')
+		wiki_dump.write("\n==Gold==\n")
+		wiki_dump.write("List of every item used to get more max gold:\n")
+		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[5, "'gold.max' in cell"]]))
 
-		for json_value in result_list:
-			furniture_json = furniture_info(json_value)
+		wiki_dump.write("\n==Research==\n")
+		wiki_dump.write("List of every item used to get more max research:\n")
+		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[5, "'scrolls.max' in cell or 'codices.max' in cell or 'tomes.max' in cell"]]))
 
-			wiki_dump.write('|-\n')
-			# NAME part
-			if furniture_json.get('sym') != None:
-				wiki_dump.write('| <span id="' + str(furniture_json['id']) + '">' + furniture_json['sym'] + '[[' +  str(furniture_json['name']) + ']]</span> ||')
-			else:
-				wiki_dump.write('| <span id="' + str(furniture_json['id']) + '">[[' +  str(furniture_json['name']) + ']]</span> ||')
+		wiki_dump.write("\n==Source==\n")
+		wiki_dump.write("List of every item used to be able to discover or use a skill:\n")
+		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[2, "'source' in cell"]]))
 
-			wiki_dump.write(str(furniture_json['desc']) + ' || ')
+		wiki_dump.write("\n==Skill max==\n")
+		wiki_dump.write("List of every item used to get a higher skill level:\n")
+		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[5, "'lore.max' in cell"]]))
 
-			for tag in furniture_json['tags']:
-				wiki_dump.write(str(tag) + "<br/>")
-			wiki_dump.write(' || ')
-
-			wiki_dump.write(str(furniture_json['base_max'] ) + ' || ')
-
-			for mod_key in furniture_json['cost']:
-				wiki_dump.write(str(mod_key) + ": " + str(furniture_json['cost'][mod_key]) + '<br/>')
-			wiki_dump.write(' || ')
-			
-			for mod_key in furniture_json['mod']:
-				wiki_dump.write(str(mod_key) + ": " + str(furniture_json['mod'][mod_key]) + '<br/>')
-
-			wiki_dump.write(' || ')
-
-			wiki_dump.write(str(furniture_json['require'].replace("&&", "<br/>").replace("||", "<br/>OR<br/>")))
-
-			wiki_dump.write('\n')
-		wiki_dump.write('|}')
+		wiki_dump.write("\n==Full List==\n")
+		wiki_dump.write(wiki.make_table(table_keys, table_lines))
