d4416e685dd78a3842f967e72cd18d03082f2e9e
lamphobic <kongregamer@gmail.com>
2020-01-20 10:24:25 -0800

    all current main files now have requirements parsing. added skills.

--- lib/skill.py
+++ lib/skill.py
@@ -5,7 +5,7 @@ Contributors: Lamphobic
 Purpose: Produce all pages directly related to skills.
 """
 
-import os, json, sys, datetime
+import os, json, sys, datetime, re
 
 import lib.extractlib as lib
 import lib.action as action
@@ -93,6 +93,93 @@ def skill_info(skill_json):
 		skill['require'] = skill_json.get('require')
 	else: 
 		skill['require'] = "Nothing"
+		
+	
+	
+	skill['requirements'] = {}
+	skill['requirements']['>'] = {}
+	skill['requirements']['<'] = {}
+	requirements = skill['requirements']
+	if skill_json.get('require') is not None:
+		require = skill_json.get('require')
+		if isinstance(require, list):
+			for e in require:
+				if not isinstance(e, dict):
+					requirements['>'][e] = 1
+				else:
+					for ent in e:
+						requirements['>'][ent] = e[ent]
+		elif isinstance(require, dict):
+			for e in require:
+				requirements['>'][e] = require[e]
+		else:
+			require = require.replace('(', '').replace(')', '').replace('g.', '')
+			for e in re.split('&&|\|\|', require):
+				if '+' in e:
+					reg = '>=|<=|>|<'
+					cmp_sign = str(re.search(reg, e).group())
+					tmp = re.split(reg, e)
+					tmpl = tmp[0].split('+')
+					for ent in tmpl:
+						if '>=' == cmp_sign:
+							requirements['>'][ent] = int(tmp[1])
+						elif '>' == cmp_sign:
+							requirements['>'][ent] = int(tmp[1]) + 1
+						else:
+							requirements['<'][ent] = int(tmp[1])
+							
+				elif bool(re.search('>=|>', e)):
+					if '>=' in e:
+						s = e.split('>=')
+						requirements['>'][s[0]] = int(s[1])
+					else:
+						s = e.split('>')
+						requirements['>'][s[0]] = int(s[1]) + 1
+				elif bool(re.search('<=|<', e)):
+					s = re.split('<=|<', e)
+					requirements['<'][s[0]] = int(s[1])
+				else:
+					requirements['>'][e] = 1
+	if skill_json.get('need') is not None:
+		require = skill_json.get('need')
+		if isinstance(require, list):
+			for e in require:
+				if not isinstance(e, dict):
+					requirements['>'][e] = 1
+				else:
+					for ent in e:
+						requirements['>'][ent] = e[ent]
+		elif isinstance(require, dict):
+			for e in require:
+				requirements['>'][e] = require[e]
+		else:
+			require = require.replace('(', '').replace(')', '').replace('g.', '')
+			for e in re.split('&&|\|\|', require):
+				if '+' in e:
+					reg = '>=|<=|>|<'
+					cmp_sign = str(re.search(reg, e).group())
+					tmp = re.split(reg, e)
+					tmpl = tmp[0].split('+')
+					for ent in tmpl:
+						if '>=' == cmp_sign:
+							requirements['>'][ent] = int(tmp[1])
+						elif '>' == cmp_sign:
+							requirements['>'][ent] = int(tmp[1]) + 1
+						else:
+							requirements['<'][ent] = int(tmp[1])
+							
+				elif bool(re.search('>=|>', e)):
+					if '>=' in e:
+						s = e.split('>=')
+						requirements['>'][s[0]] = int(s[1])
+					else:
+						s = e.split('>')
+						requirements['>'][s[0]] = int(s[1]) + 1
+				elif bool(re.search('<=|<', e)):
+					s = re.split('<=|<', e)
+					requirements['<'][s[0]] = int(s[1])
+				else:
+					requirements['>'][e] = 1
 
 	return skill
 
