629d63cd0aba00e443a75e104fc8b5f3d3b407d1
lamphobic <kongregamer@gmail.com>
2020-01-20 09:40:39 -0800

    added parsing reqs for cross referencing

--- lib/upgrade.py
+++ lib/upgrade.py
@@ -5,7 +5,7 @@ Contributors: Lamphobic
 Purpose: Produce all pages directly related to upgrades.
 """
 
-import os, json, sys, datetime
+import os, json, sys, datetime, re
 import lib.extractlib as lib
 import lib.wikilib as wiki
 
@@ -73,6 +73,82 @@ def upgrade_info(upgrade_json):
 	if upgrade_json.get('mod') is not None:
 		upgrade['effect']['mod']  = upgrade_json.get('mod')
 		upgrade['mod'] = upgrade_json.get('mod')
+		
+	upgrade['requirements'] = {}
+	upgrade['requirements']['>'] = {}
+	upgrade['requirements']['<'] = {}
+	requirements = upgrade['requirements']
+	if upgrade_json.get('require') is not None:
+		require = upgrade_json.get('require')
+		if isinstance(require, list):
+			for e in require:
+				requirements['>'][e] = 1
+		else:
+			require = require.replace('(', '').replace(')', '').replace('g.', '')
+			for e in re.split('&&|\|\|', require):
+				if '+' in e:
+					reg = '>=|<=|>|<'
+					cmp_sign = str(re.search(reg, e).group())
+					tmp = re.split(reg, e)
+					tmpl = tmp[0].split('+')
+					for ent in tmpl:
+						if '>=' == cmp_sign:
+							requirements['>'][ent] = int(tmp[1])
+						elif '>' == cmp_sign:
+							requirements['>'][ent] = int(tmp[1]) + 1
+						else:
+							requirements['<'][ent] = int(tmp[1])
+							
+				elif bool(re.search('>=|>', e)):
+					if '>=' in e:
+						s = e.split('>=')
+						requirements['>'][s[0]] = int(s[1])
+					else:
+						s = e.split('>')
+						requirements['>'][s[0]] = int(s[1]) + 1
+				elif bool(re.search('<=|<', e)):
+					s = re.split('<=|<', e)
+					requirements['<'][s[0]] = int(s[1])
+				else:
+					requirements['>'][e] = 1
+	if upgrade_json.get('need') is not None:
+		require = upgrade_json.get('need')
+		if isinstance(require, list):
+			for e in require:
+				requirements['>'][e] = 1
+		else:
+			require = require.replace('(', '').replace(')', '').replace('g.', '')
+			for e in re.split('&&|\|\|', require):
+				if '+' in e:
+					reg = '>=|<=|>|<'
+					cmp_sign = str(re.search(reg, e).group())
+					tmp = re.split(reg, e)
+					tmpl = tmp[0].split('+')
+					for ent in tmpl:
+						if '>=' == cmp_sign:
+							requirements[ent] = int(tmp[1])
+						elif '>' == cmp_sign:
+							requirements[ent] = int(tmp[1]) + 1
+						else:
+							requirements[ent] = int(tmp[1])
+							
+				elif bool(re.search('>=|>', e)):
+					if '>=' in e:
+						s = e.split('>=')
+						requirements['>'][s[0]] = int(s[1])
+					else:
+						s = e.split('>')
+						requirements['>'][s[0]] = int(s[1]) + 1
+				elif '<=' in e:
+					s = e.split('<=')
+					requirements[s[0]] = int(s[1])
+				elif '<' in e:
+					s = e.split('<')
+					requirements['<'][s[0]] = int(s[1]) - 1
+				else:
+					requirements['>'][e] = 1
+					
+	
 	if upgrade_json.get('require') is not None:
 		upgrade['require'] = upgrade_json.get('require')
 	else: 
