60982be7090877cb94a25da6202b895b14eb40ad
lamphobic <kongregamer@gmail.com>
2020-01-21 08:28:46 -0800

    cleanup table output for actions

--- lib/action.py
+++ lib/action.py
@@ -9,15 +9,6 @@ import os, json, sys, datetime, re
 import lib.extractlib as lib
 import lib.wikilib as wiki
 
-def extract_effect_key(effect_json):
-	return_txt = ""
-	if effect_json is not None:
-		if (effect_json,str):
-			return_txt += str(effect_json)
-		else:
-			for mod_list in effect_json:
-				return_txt += " * " + str(mod_list) + ": " + str(extract_effect_key(effect_json[mod_list])) + "<br/>"
-	return return_txt
 
 def get_effect_info(effect_json):
 	return_txt = ""
@@ -25,16 +16,20 @@ def get_effect_info(effect_json):
 	for effect_key in effect_json:
 		if isinstance(effect_json[effect_key], str):
 			return_txt += str(effect_json[effect_key])
+		elif isinstance(effect_json[effect_key], list):
+			return_txt += ', '.join(effect_json[effect_key])
 		else:
-			if isinstance(effect_json[effect_key], list):
-				for effect_item in effect_json[effect_key]:
-					return_txt += str(effect_item) + ", "
-				return_txt = return_txt[:-2]
-			else:
-				for effect_name in effect_json[effect_key]:
-					return_txt += " * " + str(effect_name) + ": " + str(extract_effect_key(effect_json[effect_key][effect_name])) + "<br/>"
+			for effect_name in effect_json[effect_key]:
+				if not isinstance(effect_json[effect_key][effect_name], dict):
+					return_txt += str(effect_name) + ": " + str(effect_json[effect_key][effect_name]) + "<br/>"
+				else:
+					print('asdalskdfjhglaskj')
+					print(effect_json)
+					for mod_entry in effect_json[effect_key][effect_name]:
+						return_txt += str(effect_name) + '.' + str(mod_entry) + ": " + str(effect_json[effect_key][effect_name][mod_entry]) + "<br/>"
 	return return_txt
 
+	
 def action_info(action_json):
 #Get every information of a action:
 #ID, name, description, cost, length, repeatable, effect, upgrade, require
@@ -57,7 +52,7 @@ def action_info(action_json):
 		elif isinstance(action_json.get('cost'), str):
 			action['cost']['give'] = action_json.get('cost')
 		else:
-			action['cost']  = action_json.get('cost')
+			action['cost'] = action_json.get('cost')
 
 	action['run'] = {}
 	if action_json.get('run') is not None:
@@ -66,36 +61,42 @@ def action_info(action_json):
 		elif isinstance(action_json.get('run'), str):
 			action['run']['give'] = action_json.get('run')
 		else:
-			action['run']  = action_json.get('run')
+			action['run'] = action_json.get('run')
 
 	if action_json.get('length') is not None:
-		action['length']  = action_json.get('length')
+		action['length'] = action_json.get('length')
+	elif action_json.get('perpetual'):
+		action['length'] = "Perpetual"
 	else:
-		action['length']  = "Instant"
+		action['length'] = "Instant"
 
 	if action_json.get('repeat') is not None:
-		action['repeat']  = action_json.get('repeat')
+		action['repeat'] = action_json.get('repeat')
 	else:
-		action['repeat']  = True
+		action['repeat'] = True
 		
 	
 	action['mod'] = {}
-	action['effect']  = {}
+	action['effect'] = {}
+	action['result'] = {}
+	
 	if action_json.get('effect') is not None:
-		action['effect']['effect']  = action_json.get('effect')
+		if action['length'] is "Instant":
+			action['result']['effect'] = action_json.get('effect')
+		else:
+			action['effect']['effect'] = action_json.get('effect')
 		action['mod'].update(action_json.get('effect'))
-
-	action['result']  = {}
+	
 	if action_json.get('mod') is not None:
-		action['result']['mod']  = action_json.get('mod')
+		action['result']['mod'] = action_json.get('mod')
 		action['mod'].update(action_json.get('mod'))
 	if action_json.get('result') is not None:
-		action['result']['result']  = action_json.get('result')
+		action['result']['result'] = action_json.get('result')
 		if isinstance(action_json.get('result'), dict):
 			action['mod'].update(action_json.get('result'))
-		
+	
 	is_done = False
-	action['upgrade']= {}
+	action['upgrade'] = {}
 	if action_json.get('at') is not None:
 		is_done = True
 		action['upgrade']['at'] = action_json.get('at')
@@ -104,7 +105,7 @@ def action_info(action_json):
 		action['upgrade']['every'] = action_json.get('every')
 	if is_done is False:
 		action['upgrade'] = "No"
-
+	
 	action['requirements'] = {}
 	action['requirements']['>'] = {}
 	action['requirements']['<'] = {}
@@ -187,7 +188,6 @@ def action_info(action_json):
 	return action
 
 
-
 def get_full_action_list():
 	result_list = lib.get_json("data/", "actions")
 	action_list = list()
@@ -195,63 +195,66 @@ def get_full_action_list():
 		action_list.append(action_info(json_value))
 	return action_list
 
+	
 def generate_wiki():
 	table_keys = ['Name', 'Description', 'Cost', 'Length', 'Repeatable', 'Effect', 'Upgrade', 'Requirement'] 
+	table_keys = ['Name', 'Description', 'Start Cost', 'Ongoing Cost', 'Length', 'Repeatable', 'Ongoing Effect', 'Result', 'Upgrade', 'Unlock Requirements'] 
 	table_lines = []
 	school_set = set()
 	result_list = lib.get_json("data/", "actions")
+	result_list = sorted(result_list, key=lambda act: act.get('id').title() if act.get('name') is None else act.get('name').title()) #Presorts results by name.
 	for json_value in result_list:
 		action_json = action_info(json_value)
 		table_line = []
 		# NAME part
 		if action_json.get('sym') is not None:
-			table_line.append('| <span id="' + str(action_json['id']) + '">' + action_json['sym'] + '[[' +  str(action_json['name']).capitalize() + ']]</span>')
+			table_line.append('| <span id="' + str(action_json['id']) + '">' + action_json['sym'] + '[[' +  str(action_json['name']) + ']]</span>')
 		else:
-			table_line.append('| <span id="' + str(action_json['id']) + '">[[' +  str(action_json['name']).capitalize() + ']]</span>')
+			table_line.append('| <span id="' + str(action_json['id']) + '">[[' +  str(action_json['name']) + ']]</span>')
 
 		# Description part
 		table_line.append(str(action_json['desc']))
 
-		# cost part
+		#Start Cost
 		tmp_cell = ""
 		if bool(action_json['cost']) is not False:
 			if isinstance(action_json['cost'],str):
-				tmp_cell += "To start action:<br/>"
-				tmp_cell += " * " + (str(action_json['cost']))
+				tmp_cell += (str(action_json['cost']))
 			elif isinstance(action_json['cost'], int):
-				tmp_cell += "To start action:<br/>"
-				tmp_cell += " * " + ("Gold: " + str(action_json['cost']))
+				tmp_cell += ("Gold: " + str(action_json['cost']))
 			else:
-				tmp_cell += "To start action:<br/>"
 				for mod_key in action_json['cost']:
-					tmp_cell += " * " + (str(mod_key) + ": " + str(action_json['cost'][mod_key]) + '<br/>')
+					tmp_cell += (str(mod_key) + ": " + str(action_json['cost'][mod_key]) + '<br/>')
+		table_line.append(str(tmp_cell))
 
+		#Ongoing Cost
+		tmp_cell = ""
 		if bool(action_json['run']) is not False:
 			if isinstance(action_json['run'],str):
-				tmp_cell += "To run action:<br/>"
-				tmp_cell += " * " + (str(action_json['run']))
+				tmp_cell += (str(action_json['run']))
 			elif isinstance(action_json['run'], int):
-				tmp_cell += "To run action:<br/>"
-				tmp_cell += " * " + ("Gold: " + str(action_json['run']))
+				tmp_cell += ("Gold: " + str(action_json['run']))
 			else:
-				tmp_cell += "To run action:<br/>"
 				for mod_key in action_json['run']:
-					tmp_cell += " * " + (str(mod_key) + ": " + str(action_json['run'][mod_key]) + '<br/>')
-
+					tmp_cell += (str(mod_key) + ": " + str(action_json['run'][mod_key]) + '<br/>')
 		table_line.append(str(tmp_cell))
 
 		# Length part
 		table_line.append(str(action_json['length']))
 
-		# Length part
+		# Repeatable part
 		table_line.append(str(action_json['repeat']))
 
-		# Effect part
+		# Ongoing Effect part
+		tmp_cell = ""
+		if bool(action_json['effect']) is not False:
+			tmp_cell += str(get_effect_info(action_json['effect']))
+		table_line.append(tmp_cell)
+		
+		# Result part
 		tmp_cell = ""
 		if bool(action_json['result']) is not False:
-			tmp_cell += "Finishing effect: <br/>" + str(get_effect_info(action_json['result']))
-		if bool(action_json['effect']) is not False:
-			tmp_cell += "Running effect: <br/>" + str(get_effect_info(action_json['effect']))
+			tmp_cell += str(get_effect_info(action_json['result']))
 		table_line.append(tmp_cell)
 
 		# Upgrade part 
@@ -264,13 +267,13 @@ def generate_wiki():
 					tmp_cell += ("After " + str(level_key) + " uses:<br/>")
 					level_json = action_json['upgrade']['at'].get(level_key)
 					for result_key in level_json:
-						tmp_cell += " * " + str(result_key) + ": " + str(level_json[result_key]) + "<br/>"
+						tmp_cell += "-" + str(result_key) + ": " + str(level_json[result_key]) + "<br/>"
 			if action_json['upgrade'].get('every') is not None:
 				for level_key in action_json['upgrade'].get('every'):
 					tmp_cell += ("Every " + str(level_key) + " uses:<br/>")
 					level_json = action_json['upgrade']['every'].get(level_key)
 					for result_key in level_json:
-						tmp_cell += " * " + str(result_key) + ": " + str(level_json[result_key]) + "<br/>"
+						tmp_cell += "-" + str(result_key) + ": " + str(level_json[result_key]) + "<br/>"
 
 		table_line.append(str(tmp_cell))
 
@@ -283,13 +286,13 @@ def generate_wiki():
 		wiki_dump.write('This page has been automatically updated the ' + str(datetime.datetime.now()) + "<br/>\n__FORCETOC__\n")
 
 		wiki_dump.write("\n==Instant actions==\n")
-		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[3, "'Instant' in cell"]]))
+		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[4, "'Instant' in cell"]]))
 
 		wiki_dump.write("\n==Time consuming actions==\n")
-		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[3, "not('Instant' in cell)"]]))
+		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[4, "'Instant' not in cell"]]))
 
 		wiki_dump.write("\n==One time actions==\n")
-		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[4, "'False' in cell"]]))
+		wiki_dump.write(wiki.make_table(table_keys, table_lines, table_filter=[[5, "'False' in cell"]]))
 
 		wiki_dump.write("\n==Full List==\n")
 		wiki_dump.write(wiki.make_table(table_keys, table_lines))
